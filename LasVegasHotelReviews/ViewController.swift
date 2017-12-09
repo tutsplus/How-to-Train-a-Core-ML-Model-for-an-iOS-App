2841306b87ea6585e7f778180f31df0065161f17
Doron Katz <doron@doronkatz.com>
2017-12-08 17:40:09 -0800

    First version of CoreML predictions engine for Vegas Reviews

--- LasVegasHotelReviews/ViewController.swift
+++ LasVegasHotelReviews/ViewController.swift
@@ -8,18 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController{
 
+    let reviews = Vegas_Reviews()
+    
+    @IBOutlet weak var stackView: UIStackView!
+    
+    @IBOutlet weak var scoreValue: UILabel!
+    @IBOutlet weak var nrReviews: UISlider!
+    @IBOutlet weak var nrHotelReviews: UISlider!
+    @IBOutlet weak var stars: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.makePrediction(self)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @IBAction func makePrediction(_ sender: Any){
+        let nrReviewsSelected = Double(nrReviews.value)
+        let nrHotelReviewsSelected = Double(nrHotelReviews.value)
+        
+        var starsSelected: Double{
+            switch stars.selectedSegmentIndex {
+            case 0:
+                return 3.0
+            case 1:
+                return 4.0
+            case 2:
+                return 5.0
+            default:
+                return 5.0
+            }
+        }
+        
+        if let predictions = try? self.reviews.prediction(Nr__reviews: nrReviewsSelected, Nr__hotel_reviews: nrHotelReviewsSelected, Hotel_stars: starsSelected){
+
+            let scoreFormatter = NumberFormatter()
+            scoreFormatter.numberStyle = .decimal
+            scoreFormatter.maximumFractionDigits = 1
+            self.scoreValue.text = scoreFormatter.string(for: predictions.Score)
+
+        }else{
+            print("Error")
+        }
+        
+        
+        
+    }
 
 
 }
+
 
